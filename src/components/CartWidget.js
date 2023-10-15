92d8e01bf1109641b65165b7547e379a0a38e3c1
unknown <fonchiruiz@hotmail.com>
2023-10-15 13:45:38 -0300

    Primer commit con mis archivos

---
+++ src/components/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CartWidget = () => {
+  const cartItemCount = 5;
+
+  return (
+    <div className="cart-widget">
+      <i className="fas fa-shopping-cart"></i>
+      <span className="cart-item-count">{cartItemCount}</span>
+    </div>
+  );
+};
+
+export default CartWidget;
